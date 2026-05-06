b8bc55340c747befeecd06f3a92b3fb5c7150387
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2026-05-06 09:17:46 +0600

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,31 +18,52 @@ class UserSeeder extends Seeder
             $clients->personalAccessClient(config('auth.guards.api.provider', 'users'));
         } catch (\RuntimeException) {
             $clients->createPersonalAccessGrantClient(
-                (string) config('app.name').' Personal Access Client',
+                (string) config('app.name') . ' Personal Access Client',
                 config('auth.guards.api.provider', 'users')
             );
         }
 
+        User::query()->firstOrCreate(
+            ['email' => 'm.alexpersad@gmail.com'],
+            [
+                'name' => 'Alex Persad',
+                'phone' => null,
+                'locale' => 'en',
+                'password' => Hash::make('password'),
+                'role' => UserRole::ADMIN,
+                'email_verified_at' => now(),
+            ]
+        );
+        User::query()->firstOrCreate(
+            ['email' => 'mahfuz.maktech@gmail.com'],
+            [
+                'name' => 'Mahfuz Ahmed',
+                'phone' => null,
+                'locale' => 'en',
+                'password' => Hash::make('password'),
+                'role' => UserRole::ADMIN,
+                'email_verified_at' => now(),
+            ]
+        );
+        User::query()->firstOrCreate(
+            ['email' => 'monirul.maktech@gmail.com'],
+            [
+                'name' => 'Monirul Islam',
+                'phone' => null,
+                'locale' => 'en',
+                'password' => Hash::make('password'),
+                'role' => UserRole::ADMIN,
+                'email_verified_at' => now(),
+            ]
+        );
         User::query()->firstOrCreate(
             ['email' => 'admin@dev.com'],
             [
                 'name' => 'Admin',
                 'phone' => null,
                 'locale' => 'en',
-                'password' => Hash::make('admin@dev.com'),
+                'password' => Hash::make('password'),
                 'role' => UserRole::ADMIN,
-                'email_verified_at' => now(),
-            ]
-        );
-
-        User::query()->firstOrCreate(
-            ['email' => 'user@dev.com'],
-            [
-                'name' => 'Demo User',
-                'phone' => null,
-                'locale' => 'en',
-                'password' => Hash::make('user@dev.com'),
-                'role' => UserRole::USER,
                 'email_verified_at' => now(),
             ]
         );
